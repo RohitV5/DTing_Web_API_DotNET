a15a7a6ec63899bf3915421bfd32dc71b2542ec0
Rohit Verma <verma.rohit.in@gmail.com>
2023-04-15 15:21:46 +0530

    feature | ngform

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent {
+  model: { username: string; password: string } = {
+    username: '',
+    password: '',
+  };
+
+  login() {
+    console.log(this.model);
+  }
+}

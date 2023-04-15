d8b7c852a91eeaaaa01177efbe4053af2ee0bc42
Rohit Verma <verma.rohit.in@gmail.com>
2023-04-15 17:39:59 +0530

    feature | user loggedin async pipe in template

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,3 +1,5 @@
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -6,41 +8,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit{
+export class NavComponent implements OnInit {
   model: { username: string; password: string } = {
     username: '',
     password: '',
   };
 
-  loggedIn: boolean = false;
+  currentUser$: Observable<User | null> = of(null); //initialize with null;
 
   constructor(private accountService: AccountService) {}
 
-  ngOnInit(){
-    this.getCurrentUser()
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: (user) => {
-        this.loggedIn = !!user;
-      },
-      error: (error) => console.log(error),
-    });
+  ngOnInit() {
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.loggedIn = true;
-      },
+      next: (response) => {},
       error: (error) => console.log(error),
     });
   }
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 }

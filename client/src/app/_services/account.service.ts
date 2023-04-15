d16d821d1e25dc55948358bd39e01ccae61a41c2
Rohit Verma <verma.rohit.in@gmail.com>
2023-04-15 15:28:38 +0530

    feature | Account login service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+//Service is a singleton that is its instantiated when application starts and destroyed when applicaiton is closed
+//Hence we can save state which we want our application to remember
 export class AccountService {
+  baseUrl = 'http://localhost:5186/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model);
+  }
 }

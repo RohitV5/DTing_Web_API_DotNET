ddf2fbd0893f7ad2a915a5943b2bfbb5931b61ce
Rohit Verma <verma.rohit.in@gmail.com>
2023-04-22 08:57:38 +0530

    feature : Extending Entity AppUser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,13 +3,42 @@ namespace API.Entities
     public class AppUser
     {
         //Entity framework is convention based and it will make Id as primary key in table
-        public int Id {get;set;}
+        public int Id { get; set; }
 
         public string UserName { get; set; }
 
-        public byte[] PasswordHash {get;set;}
+        public byte[] PasswordHash { get; set; }
 
-        public byte[] PasswordSalt {get;set;}
+        public byte[] PasswordSalt { get; set; }
+
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = new();  //shorthand for new List<Photo>()
+
+
+
+
+
 
     }
 }
